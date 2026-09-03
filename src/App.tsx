2f5801023c8c318a79236d22eff37a01f1031157
yared <162388604+yared22@users.noreply.github.com>
2026-09-03 12:13:41 +0300

    asdasd

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import Index from "./pages/Index";
 import ComponentsGallery from "./pages/ComponentsGallery";
 import NotFound from "./pages/NotFound";
@@ -26,6 +28,8 @@ const App = () => (
         </BrowserRouter>
       </TooltipProvider>
     </ThemeProvider>
+    <Analytics />
+    <SpeedInsights />
   </QueryClientProvider>
 );
 
